26bc3a7b36a0d8231ce06b1a535dd8b2ce85b01e
Enrico Coppolecchia <enrycoop92@gmail.com>
2023-12-09 00:43:48 +0100

    zrythm builder

---
+++ install-docker.sh
@@ -0,0 +1,11 @@
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine \
+                  ocker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
